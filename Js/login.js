8cdc20a8a1c56a6e0289d89043dc8f5716561441
Xurshid97 <fantasticidea3000@gmail.com>
2023-08-19 11:47:03 +0200

    Login page alert

--- Js/login.js
+++ Js/login.js
@@ -15,9 +15,9 @@ let data = fetch(apiUsers)
                 }
                 else {                    
                     // set user data to local storage of browser
-                
+                    
                     const userData = {
-                        username: element.username,
+                        username: loginEl['username'].value,
                         itemdata: []
                     }
 
