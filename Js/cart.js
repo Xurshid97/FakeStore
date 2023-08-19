2e4bec4e10fabe760fcdb28a6ac021c96e8881c1
Xurshid97 <fantasticidea3000@gmail.com>
2023-08-19 09:33:10 +0200

    Updated version of store with design

--- Js/cart.js
+++ Js/cart.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
         a: create map to every element id as key, and its number as value
         b: update value every time + button pressed, otherwise - pressed
         c: if remove button pressed remove current element from map and array
+        d: update cart_calculator where total cost and number of items will be shown
     */
     let localStorageData = JSON.parse(localStorage.getItem('userData')) 
     let allItemsOfCart = localStorageData.itemdata
@@ -18,7 +19,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
             let item_card = document.querySelector('.items_page')      
             data.filter((element)=>{
                 allItemsOfCart.forEach(e => {
-
                     if (element.id == e[0]) {
                         let order_infoEl = document.createElement('item_card')
                         order_infoEl.className = 'item_card'
@@ -33,13 +33,15 @@ document.addEventListener('DOMContentLoaded', ()=>{
                                                         <button class = 'subtr' data_id="${e[0]}">-</button>
                                                         <h6 class = 'presentNumOfEls'>${e[1]}</h6>
                                                         <button class = 'add' data_id="${e[0]}">+</button>
-                                                        <button>Remove</button>
+                                                        <button class = 'remove'>Remove</button>
                                                     </div>
                                                 `
-                        item_card.appendChild(order_infoEl)                        
+                        item_card.appendChild(order_infoEl)
                     }                    
                 });
             })
+
+
             // let add = document.querySelectorAll('.add')
             let item_counterEls = document.querySelectorAll('.item_counter')
             
@@ -58,6 +60,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
                             }
 
                             localStorage.setItem('userData', JSON.stringify(updateStorage))
+                            location.reload()
                         }
                     })                    
                 })
@@ -67,23 +70,66 @@ document.addEventListener('DOMContentLoaded', ()=>{
                     allItemsOfCart.filter((idEl)=>{
                         if (idEl[0] == e.getAttribute('data_id')) {
                             idEl[1]--
-                            if (idEl[1] == 0) {
-                                let indexOfRemover = allItemsOfCart.indexOf(idEl)
-                                console.log(indexOfRemover);                                
-                            }
                             presentNumOfEls.innerHTML = `${idEl[1]}`
 
+                            // if item is left 0 it will be removed
+                            if (idEl[1] == 0) {
+                                let indexOfRemover = allItemsOfCart.indexOf(idEl)
+                                allItemsOfCart.splice(indexOfRemover, 1)
+
+                                window.location.reload()                           
+                            }
+                            
                             let updateStorage = {
                                 username: localStorageData['username'],
                                 itemdata: allItemsOfCart
                             }
-
                             localStorage.setItem('userData', JSON.stringify(updateStorage))
+
+                            location.reload()
                         }
                     })                    
                 })
 
+                let removeBtn = e.querySelector('.remove')
+                removeBtn.addEventListener('click', ()=>{
+                    allItemsOfCart.filter((idEl)=>{
+                        if (idEl[0] == e.getAttribute('data_id')) {
+                            let indexOfRemover = allItemsOfCart.indexOf(idEl)
+                            allItemsOfCart.splice(indexOfRemover, 1)
+                            
+                            let updateStorage = {
+                                username: localStorageData['username'],
+                                itemdata: allItemsOfCart
+                            }
+                            localStorage.setItem('userData', JSON.stringify(updateStorage))
+                            location.reload()
+                        }
+                    })                    
+                })                
+            })  
+            
+
+            // total number of items in cart
+            let items_numberEl = document.querySelector('.items_number')
+            let counter = 0
+            allItemsOfCart.filter((item)=>{
+                counter = counter + item[1]
+            })            
+            items_numberEl.innerHTML = `Soni umumiy ${counter} ta `
+
+            // calculating and rendering total price
+            let total_costEl = document.querySelector('.total_cost')
+            let totalPrice = 0
+            data.forEach((itemPrice)=>{
+                allItemsOfCart.forEach((itemCart)=>{
+                    if(itemCart[0] == itemPrice.id){
+                        totalPrice = totalPrice + Math.round(itemPrice.price * itemCart[1])
+                    }
+                })                
             })
+
+            total_costEl.innerHTML = `Umumiy narx ${totalPrice}$`
         })
 })
 
